93b30d557e0ef29ddd7f5214fd2f94b4e8bd7767
Vera <vera@DESKTOP-QPRLS4I.localdomain>
2022-06-07 20:03:00 +0300

    script.js

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
 const h2 = document.createElement("h2");
 h2.textContent = "This content added by JavaScript";
-document.querySelector("body").appendChild(h2);
+document.querySelector(".part1").appendChild(h2);
+let link = document.getElementsById("href")
+let change = document.getElementsById("change") 
+
+change.onpoint = function() {
+    link.onpoint
+}
