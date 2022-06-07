5bea4f6ee3278ca6adc160edc5be05c2241822ba
Vera <vera@DESKTOP-QPRLS4I.localdomain>
2022-06-07 21:25:35 +0300

    script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const h2 = document.createElement("h2");
-h2.textContent = "This content added by JavaScript";
+h2.textContent = "Tech life changing the universe";
 document.querySelector(".part1").appendChild(h2);
 let link = document.getElementsById("href")
 let change = document.getElementsById("change") 
